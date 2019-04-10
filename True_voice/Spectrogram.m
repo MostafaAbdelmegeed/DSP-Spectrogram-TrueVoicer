60903a8b8888a0e9bc773dc798f33025d5723cc9
Mustafa Asaad <mostafa0as3d@gmail.com>
2019-04-10 20:44:34 +0200

    Update Spectrogram.m

--- True_voice/Spectrogram.m
+++ True_voice/Spectrogram.m
@@ -1,7 +1,9 @@
  [y,Fs] = audioread('Emmanuel.mp3');
  [y2,Fs2] = audioread('Emmanuel_1sec.mp3');
  s = spectrogram(y); %sot emmanuel el30 sania 3la b3d
+ 
+ figure(1)
  spectrogram(y,'yaxis');
- figure
+ figure(2)
  s2=spectrogram(y2);%sot emmanuel el sania wahda
  spectrogram(y2,'yaxis');
