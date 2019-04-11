67bdea5cab1a21612869f3f6fe4e036c9d9408f0
Mustafa Asaad <mostafa0as3d@gmail.com>
2019-04-11 12:42:46 +0200

    Finished main algorithms!

--- True_voice/spectrogram_reading.m
+++ True_voice/spectrogram_reading.m
@@ -1,8 +1,16 @@
-% Ui dialog open box to browse the Spectrograph 
-[spectrogram_filename,spectrogram_path] = uigetfile('*.jpg;*.jpeg;*.png','Select Spectrograph');
-spectrogram_file = [spectrogram_path spectrogram_filename]; % Reading the file
-spectrogram = rgb2gray(imread(spectrogram_file)); % Reading the image in grayscale
 
+% Ui dialog open box to browse the Spectrograph 
+[spectrogram_filename,spectrogram_path] = uigetfile('*.jpg;*.jpeg;*.png','Select Spectrograms','MultiSelect','on');
+spectrogram_file1 = [spectrogram_path char(spectrogram_filename(1))]; % Reading the file
+spectrogram_file2 = [spectrogram_path char(spectrogram_filename(2))]; % Reading the file
+spectrogram_file3 = [spectrogram_path char(spectrogram_filename(3))]; % Reading the file
+spectrogram_file4 = [spectrogram_path char(spectrogram_filename(4))]; % Reading the file
+
+
+spectrogram1 = rgb2gray(imread(spectrogram_file1)); % Reading the image in grayscale
+spectrogram2 = rgb2gray(imread(spectrogram_file2));
+spectrogram3 = rgb2gray(imread(spectrogram_file3));
+spectrogram4 = rgb2gray(imread(spectrogram_file4));
 
 % Ui dialog open box to browse the Template
 [template_filename,template_path] = uigetfile('*.jpg;*.jpeg;*.png','Select Template'); 
--- True_voice/test.m
+++ True_voice/test.m
@@ -1,5 +1,5 @@
-[s,Fss] = audioread('gamila20sec.mp3');
+[s,Fss] = audioread('gamila5.mp3');
 s(:,2)=[];
 y= spectrogram(s);
 spectrogram(s,[],[],[],Fss,'yaxis');
-saveas(gcf,'Gamila spectrogram.png');
+saveas(gcf,'gamila template.png');
