02d6c835e0da52ad3f3b21102fbbd31bb7bdf9e1
Mustafa Asaad <mostafa0as3d@gmail.com>
2019-04-11 14:28:14 +0200

    Adding samples!

--- True_voice/test.m
+++ True_voice/test.m
@@ -1,5 +1,5 @@
-[s,Fss] = audioread('Eman17.mp3');
-%s(:,2)=[];
+[s,Fss] = audioread('Merna_1sec.mp3');
+s(:,2)=[];
 y= spectrogram(s);
 spectrogram(s,[],[],[],Fss,'yaxis');
-saveas(gcf,'Eman template(2).png');
+saveas(gcf,'Merna template.png');
